e0daf6b03c6bb962405e4c016cc907273f48a537
lixiangyuangroup <109457122+lixiangyuangroup@users.noreply.github.com>
2022-07-21 10:50:20 +0800

    Add files via upload

--- TDOC/src/_CBH.py
+++ TDOC/src/_CBH.py
@@ -1,19 +1,50 @@
 # -*- coding: utf-8 -*-
 from collections import Counter
 from rdkit import Chem
-
+from itertools import chain
+from rdkit.Chem import AllChem
 
 class CBH(object):
+
+    def __init__(self,aro_rings=['C12=CC=CC=C1C=CC=C2','C1=CC=CC=C1','C1=COC=C1']):
+        self.replaced_unit=['S','P','As']
+        self.aro_rings=aro_rings
+    
+    def replace_mol(self, mol):
+        Chem.Kekulize(mol, clearAromaticFlags = True)
+        for i, v in enumerate(self.aro_rings):
+            smarts = Chem.MolFromSmarts(v)
+            units = Chem.MolFromSmarts(self.replaced_unit[i])
+            mol = AllChem.ReplaceSubstructs(mol, smarts , units, replaceAll = True)[0]
+        return mol
+
+    def recover_mol(self, mol):
+        Chem.Kekulize(mol, clearAromaticFlags = True)
+        for i, v in enumerate(self.aro_rings):
+            smarts = Chem.MolFromSmarts(v)
+            units = Chem.MolFromSmarts(self.replaced_unit[i])
+            mol = AllChem.ReplaceSubstructs(mol, units , smarts, replaceAll = True)[0]
+        return mol
+
     def CBH0_Counter(self, smi):
         smiles, mol = [], Chem.MolFromSmiles(smi)
-        if mol.GetNumHeavyAtoms() <= 1: return Counter([smi])
-        for x in mol.GetAtoms(): smiles.append(x.GetSymbol())
+        if mol.GetNumHeavyAtoms() <= 1:
+            return Counter([smi])
+        mol = self.replace_mol(mol)
+        for x in mol.GetAtoms():
+            smiles.append(x.GetSymbol())
+        for x in set(smiles)&set(self.replaced_unit):
+            mol = Chem.MolFromSmiles(x)
+            mol = self.recover_mol(mol)
+            newsmi = Chem.MolToSmiles(mol)
+            smiles = '\n'.join(smiles).replace(x, newsmi).split('\n')
         return Counter(smiles)
 
-    def CBH1_Counter(self, smi):
+    def CBH1_Counter(self, smi, ):
         smiles, mol = [], Chem.RWMol(Chem.MolFromSmiles(smi))
-        if mol.GetNumHeavyAtoms() <= 2: return Counter([smi])
-        if mol.GetAromaticAtoms(): Chem.Kekulize(mol, clearAromaticFlags = True)
+        if mol.GetNumHeavyAtoms() <= 2:
+            return Counter([smi])
+        mol = self.replace_mol(mol)
         for bond in mol.GetBonds():
             leftatom = bond.GetBeginAtom()
             rightatom = bond.GetEndAtom()
@@ -21,6 +52,7 @@ class CBH(object):
             newmol.AddAtom(leftatom)
             newmol.AddAtom(rightatom)
             newmol.AddBond(0, 1, bond.GetBondType())
+            newmol = self.recover_mol(newmol)
             smiles.append(Chem.MolToSmiles(newmol))
         if not smiles: smiles = [smi]
         return Counter(smiles)
@@ -28,7 +60,7 @@ class CBH(object):
     def CBH2_Counter(self, smi):
         smiles, mol = [], Chem.RWMol(Chem.MolFromSmiles(smi))
         if mol.GetNumHeavyAtoms() <= 3: return Counter([smi])
-        if mol.GetAromaticAtoms(): Chem.Kekulize(mol, clearAromaticFlags = True)
+        mol = self.replace_mol(mol)
         for x in mol.GetAtoms():
             newmol = Chem.RWMol(Chem.MolFromSmiles(''))
             if x.GetDegree() == 1: continue
@@ -36,13 +68,15 @@ class CBH(object):
             for i, y in enumerate(x.GetBonds()):
                 newmol.AddAtom(y.GetOtherAtom(x))
                 newmol.AddBond(0, i + 1, y.GetBondType())
+            newmol = self.recover_mol(newmol)
             smiles.append(Chem.MolToSmiles(newmol))
         return Counter(smiles)
 
     def CBH3_Counter(self, smi):
         smiles, mol = [], Chem.RWMol(Chem.MolFromSmiles(smi))
-        if mol.GetNumHeavyAtoms() <= 4: return Counter([smi])
-        if mol.GetAromaticAtoms(): Chem.Kekulize(mol, clearAromaticFlags = True)
+        if mol.GetNumHeavyAtoms() <= 4:
+            return Counter([smi])
+        mol = self.replace_mol(mol)
         for bond in mol.GetBonds():
             leftatom = bond.GetBeginAtom()
             rightatom = bond.GetEndAtom()
@@ -57,27 +91,11 @@ class CBH(object):
             for j, z in enumerate(m for m in rightatom.GetBonds() if m.GetIdx() != bond.GetIdx()):
                 newmol.AddAtom(z.GetOtherAtom(rightatom))
                 newmol.AddBond(1, i + j + 3, z.GetBondType())
+            newmol = self.recover_mol(newmol)
             smiles.append(Chem.MolToSmiles(newmol))
         if not smiles: smiles = [smi]
         return Counter(smiles)
 
-    def CBH4_Counter(self, smi):
-        smiles, mol = [], Chem.RWMol(Chem.MolFromSmiles(smi))
-        if mol.GetAromaticAtoms(): Chem.Kekulize(mol, clearAromaticFlags = True)
-        for x in mol.GetAtoms():
-            newmol = Chem.RWMol(Chem.MolFromSmiles(''))
-            if x.GetDegree() == 1 or all(m.GetDegree() == 1 for m in x.GetNeighbors()): continue
-            newmol.AddAtom(x)
-            for y in x.GetBonds():
-                Natom = newmol.GetNumAtoms()
-                newmol.AddAtom(y.GetOtherAtom(x))
-                newmol.AddBond(0, Natom, y.GetBondType())
-                for i, z in enumerate(m for m in y.GetOtherAtom(x).GetBonds() if m.GetIdx() != y.GetIdx()):
-                    newmol.AddAtom(z.GetOtherAtom(y.GetOtherAtom(x)))
-                    newmol.AddBond(Natom, Natom + i + 1, z.GetBondType())
-            smiles.append(Chem.MolToSmiles(newmol))
-        if not smiles: smiles = [smi]
-        return Counter(smiles)
         
     def Get_CBH1(self, smi):
         left, right, CBH1 = self.CBH0_Counter(smi), Counter({}), self.CBH1_Counter(smi)
@@ -97,8 +115,3 @@ class CBH(object):
         reac = Counter([smi]) + right - left
         return reac, CBH3
 
-    def Get_CBH4(self, smi):
-        left, right, CBH4 = self.CBH3_Counter(smi), Counter({}), self.CBH4_Counter(smi)
-        for k, v in CBH4.items(): right += Counter({x: y * v for x, y in self.CBH3_Counter(k).items()})
-        reac = Counter([smi]) + right - left
-        return reac, CBH4

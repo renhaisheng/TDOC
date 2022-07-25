8a43f3e80bd34f7c9f8ac85ef1a0a3b6784eee02
lixiangyuangroup <109457122+lixiangyuangroup@users.noreply.github.com>
2022-07-25 16:49:01 +0800

    Delete cc.py

--- examples/gaussian_out/M062X/cc.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from re import findall
-
-with open('[CH2]-T.out') as f:
-	content=f.read()
-res = list(map(float, findall('before annihilation\s+(\S+?),\s+after\s+(\S+)\n', content)[-1]))
-print(res)
